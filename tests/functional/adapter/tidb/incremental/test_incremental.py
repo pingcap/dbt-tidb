309411c157d296665493ce4fdc870712fa78b295
Daemonxiao <735462752@qq.com>
2022-11-25 11:15:54 +0800

    fmt

--- tests/functional/adapter/tidb/incremental/test_incremental.py
+++ tests/functional/adapter/tidb/incremental/test_incremental.py
@@ -1,6 +1,8 @@
 import pytest
 
-from dbt.tests.adapter.incremental.test_incremental_unique_id import BaseIncrementalUniqueKey
+from dbt.tests.adapter.incremental.test_incremental_unique_id import (
+    BaseIncrementalUniqueKey,
+)
 
 
 class TestIncrementalUniqueKeyTiDB(BaseIncrementalUniqueKey):
